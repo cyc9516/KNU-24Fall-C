7ca3eca868f6740309f3ab6f99733906ca33c1f9
WonjaeLee_202402070 <leewj4656@gmail.com>
2024-10-18 00:23:23 +0900

    Update day08-2.c

--- day08/day08-2.c
+++ day08/day08-2.c
@@ -14,7 +14,7 @@ int main() {
 	fillRandom(array); //배열 랜덤 초기화
 	printArray(array); //배열 출력
 	movePointer(array); //포인터를 이용한 이동
-	
+
 	return 0;
 }
 
@@ -44,30 +44,33 @@ void movePointer(void* array) //포인터를 이용한 이동
 {
 	int x = 0, y = 0, result_x = 0, result_y = 0;
 	int array_val = 0;
-	
+
 
 	while (10 * x + y <= 99) {
-		
-		array_val = *((int*)array + x * 10 + y);
-		printf("현재 위치: (%d, %d), 배열의 값: %d\n", x, y, array_val);
-		
-		x += array_val / 10;
-		y += array_val % 10;
 
+		if (x <= 9) {
+			result_x = x;
+		}
 		if (y <= 9) {
 			result_y = y;
 		}
+
+		array_val = *((int*)array + x * 10 + y);
+		printf("현재 위치: (%d, %d), 배열의 값: %d\n", x, y, array_val);
+
+		x += array_val / 10;
+		y += array_val % 10;
+
+		
 		if (y >= 10) { //자릿수 올림
 			y -= 10;
 			x += 1;
 		}
-		if (x <= 9) {
-			result_x = x;
-		}
 		
+
 	}
 
 	printf("더 이상 이동할 수 없습니다.\n");
 	printf("종료 위치: (%d, %d), 배열의 값: %d\n", result_x, result_y, array_val);
-	
+
 }
